886569448ea036d3230d7848e8e326d2cc959f18
Arikun <arikun@localhost.localdomain>
2021-07-08 18:46:16 +0700

    Update 1.1

--- core/components/footer.blade.php
+++ core/components/footer.blade.php
@@ -1,11 +1,11 @@
  <!--   Core   -->
- <script src="./assets/js/plugins/jquery/dist/jquery.min.js"></script>
- <script src="./assets/js/plugins/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+ <script src="{{asset('assets/js/plugins/jquery/dist/jquery.min.js')}}"></script>
+ <script src="{{asset('assets/js/plugins/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
  <!--   Optional JS   -->
- <script src="./assets/js/plugins/chart.js/dist/Chart.min.js"></script>
- <script src="./assets/js/plugins/chart.js/dist/Chart.extension.js"></script>
+ <script src="{{asset('assets/js/plugins/chart.js/dist/Chart.min.js')}}"></script>
+ <script src="{{asset('assets/js/plugins/chart.js/dist/Chart.extension.js')}}"></script>
  <!--   Argon JS   -->
- <script src="./assets/js/argon-dashboard.min.js?v=1.1.2"></script>
+ <script src="{{asset('assets/js/argon-dashboard.min.js?v=1.1.2')}}"></script>
  <script src="https://cdn.trackjs.com/agent/v3/latest/t.js"></script>
  <script>
    window.TrackJS &&
